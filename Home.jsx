341da74de4a6766fb5ee5874235eeeb4dd009e81
Aayushlamsal3 <132969642+Aayushlamsal3@users.noreply.github.com>
2024-01-20 14:42:08 +0545

    Create Home.jsx

---
+++ Home.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+const Home = () => {
+  const[InpDatas,SetInpDatas]=useState()
+  const[FetchedDatas,SetFetchedDatas]=useState()
+  const InputEvent=(event)=>{
+SetInpDatas(event.target.value)
+  }
+
+  const FetchApi=async ()=>{
+    const api=`https://api.openweathermap.org/data/2.5/weather?q=${InpDatas}&units=metric&appid=77ca1dac2594a6c51399b803db6bf4bf`
+    const response=await fetch(api)
+    const result=await response.json()
+    console.log(result)
+    SetFetchedDatas(result)
+  }
+
+  return (
+    <div>
+      <input type="text" name="" id="" onChange={InputEvent} />
+      <button onClick={FetchApi}>search</button>
+  {FetchedDatas && FetchedDatas.main &&(
+ <>
+    <p>City:{FetchedDatas.name}</p>
+    <p>Temperature:{FetchedDatas.main.temp} degree Celsius</p>
+ </>
+  )}
+    </div>
+  )
+}
+
+export default Home
